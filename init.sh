8a0d93c3e679667ab6c5d0f9a4a9141c074ebbfe
yujiants <yujian@tsfintech.com>
2016-11-15 15:44:30 +0800

    init

--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 read -p "What is the name of the service?" service_name
-sed -i -e "s/{TEMPLATE_PROJECT_NAME}/$service_name/g" bin/service-name config/logback-prod.xml config/wrapper.conf
+sed -i -e "s/TEMPLATE_PROJECT_NAME/$service_name/g" bin/service-name config/logback-prod.xml config/wrapper.conf
 mv bin/service-name bin/$service_name
 mkdir logs
