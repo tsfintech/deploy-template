0c379b180df29cece172e99dab4a44b4d1e1ef14
yujiants <yujian@tsfintech.com>
2016-11-15 16:06:31 +0800

    init

--- init.sh
+++ init.sh
@@ -2,7 +2,7 @@
 BASEDIR=$(dirname "$0")
 
 read -p "What is the name of the service?" SERVICE_NAME
-sed -i -e "s/TEMPLATE_PROJECT_NAME/$SREVICE_NAME/g" bin/service-name config/logback-prod.xml config/wrapper.conf
+sed -i -e "s/TEMPLATE_PROJECT_NAME/$SERVICE_NAME/g" bin/service-name config/logback-prod.xml config/wrapper.conf
 mv bin/service-name bin/$SERVICE_NAME
 mkdir logs
 cd /etc/init.d && sudo ln -s $BASEDIR/bin/$SERVICE_NAME
